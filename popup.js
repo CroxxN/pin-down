dec0eed620c33419a8a9585fb8efa5320cf5c3dc
CroxxN <saradgaihre19@gmail.com>
2022-10-17 17:57:27 +0545

    jquery working

--- popup.js
+++ popup.js
@@ -1,37 +1,21 @@
-let pop_img = document.getElementById('pop_img');
 
-pop_img.addEventListener("click", async () => {
 
+$('#butOne').on("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: pinFun,
+        target: {tabId: tab.id},
+        files: ['assets/jquery-3.6.1.min.js']
     });
-        
+    chrome.scripting.executeScript({
+    target: { tabId: tab.id, allFrames: true},
+    function: pinFun,
+
+
+    });
   });
 
 function pinFun(){
-    
-    
-    chrome.storage.sync.get("clicked", ({clicked})=>{
-        if(clicked){
-            document.body.style.backgroundColor = '#fff000';
-            chrome.storage.sync.set({clicked: false});
-            console.log("if workd")
-        }else{
-            document.body.innerHTML += '<img src="chrome-extension://mgghjdkclnilmkagebecdmljbnimkcie/images/pin.png" id="img">';
-            document.body.innerHTML += '<img src="chrome-extension://mgghjdkclnilmkagebecdmljbnimkcie/images/magnet.png" id="down">';
-            chrome.storage.sync.set({clicked: true});
-            document.getElementsByTagName("BODY")[0].addEventListener('click',(e)=>{
-                posY = e.clientY;
-                posX = e.clientX;
-                let ele = document.getElementById('img')
-                ele.style.width = "50px"
-                ele.style.top = posY + "px"
-                ele.style.left = posX + "px"
-                console.log(posX,posY)
-            })
-        }
+    $("BODY").on("click", (e)=>{
+        console.log(e.pageX, e.pageY);
     })
-    
-}
+};
