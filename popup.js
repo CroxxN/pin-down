a16a867633b0829fe42e538ffe2b270ccdfafb2b
CroxxN <saradgaihre19@gmail.com>
2022-10-18 14:15:13 +0545

    efficient javascript

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 
 
 $('#butOne').on("click", async () => {
+    console.log("Hello")
+    console.log($().jquery);
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['assets/jquery-3.6.1.min.js']
-    });
+    // chrome.scripting.executeScript({
+    //     target: {tabId: tab.id},
+    //     files: ['assets/jquery-3.6.1.min.js']
+    // });
     chrome.scripting.executeScript({
     target: { tabId: tab.id, allFrames: true},
     function: pinFun,
@@ -17,5 +19,8 @@ $('#butOne').on("click", async () => {
 function pinFun(){
     $("BODY").on("click", (e)=>{
         console.log(e.pageX, e.pageY);
+        $("BODY").off();
     })
+
+
 };
